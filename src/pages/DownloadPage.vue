034886fd7963ec4bfb8c5876284e542154d4e64d
Ethan Chien <liangtie.qian@gmail.com>
2025-01-17 15:10:38 +0800

    feat: Add download and changelog links for KiCad version 8.0.8

--- src/pages/DownloadPage.vue
+++ src/pages/DownloadPage.vue
@@ -75,6 +75,10 @@ export default {
       "8.0.7": {
         "download": "https://down.eda.cn/data/kicad-release/kicad-huaqiu-8.0.7-x86_64.exe.zip",
         "changelog": "https://kicad.eda.cn/docs/posts/KiCad-8.0.7-Release.html"
+      },
+      "8.0.8": {
+        "download": "https://down.eda.cn/data/kicad-release/kicad-huaqiu-8.0.8-x86_64.exe.zip",
+        "changelog": "https://kicad.eda.cn/docs/posts/KiCad-8.0.8-Release.html"
       }
     });
 
